9c505144e63968c3f31ed225816724908a737c1d
Quang Bui <quangb220600@student.ctu.edu.vn>
2025-07-14 15:38:21 +0700

    Create login and register function

---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+// src/auth/jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      id: payload.sub,
+      username: payload.username,
+      role: payload.role,
+    };
+  }
+}
